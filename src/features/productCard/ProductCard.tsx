077a532af5c94503e7acc2bc80c71509624c904f
YouAreNotHere <malferety@gmail.com>
2025-02-13 21:29:40 +0500

    Запилил nav, пикчи

--- src/features/productCard/ProductCard.tsx
+++ src/features/productCard/ProductCard.tsx
@@ -1,18 +1,21 @@
-import Ring from "../../assets/Ring.jpg"
-import Rings from "../../assets/RingsOnWhiteHands.jpg"
-import ChainInBlackHand from "../../assets/ChainInBlackHand.jpg"
-import RingsOnBlackHands from "../../assets/RingsOnBlackHands.jpg"
-import ChupaChups from "../../assets/ChupaChups.jpg"
+import Ring from "../../assets/Ring.jpg";
+import Rings from "../../assets/RingsOnWhiteHands.jpg";
+import ChainInBlackHand from "../../assets/ChainInBlackHand.jpg";
+import RingsOnBlackHands from "../../assets/RingsOnBlackHands.jpg";
+import ChupaChups from "../../assets/ChupaChups.jpg";
+import "./ProductCard.scss"
 
 const ProductCard = () => {
     return(
         <section className="productCard">
-            <div className="productCard__image">
-                <img src={Ring}/>
-                <img src={Rings}/>
-                <img src={ChainInBlackHand}/>
-                <img src={RingsOnBlackHands}/>
-                <img src={ChupaChups}/>
+            <div className="productCard-images-wrapper">
+                <div className="new-product">
+                    <img src={Ring} alt="photo of ring" className="productCard__image" />
+                </div>
+                <img src={Rings} className="productCard__image" alt="photo of rings"/>
+                <img src={ChainInBlackHand} className="productCard__image" alt="photo of chain"/>
+                <img src={RingsOnBlackHands} className="productCard__image" alt="photo of rings in black hands"/>
+                <img src={ChupaChups} className="productCard__image" alt="photo of rings and chupa chups"/>
             </div>
             <div className="productCard__description"></div>
         </section>
